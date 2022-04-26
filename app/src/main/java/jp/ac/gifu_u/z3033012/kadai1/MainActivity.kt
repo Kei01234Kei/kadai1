4f55de1a8758120444e561971390a692d56d3e3a
Keisuke Ishigami <keisuke.130@icloud.com>
2022-04-27 01:36:06 +0900

    first commit

---
+++ app/src/main/java/jp/ac/gifu_u/z3033012/kadai1/MainActivity.kt
@@ -0,0 +1,20 @@
+package jp.ac.gifu_u.z3033012.kadai1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val editText = findViewById<EditText>(R.id.edit_text)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            Toast.makeText(this, editText.text.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+}
