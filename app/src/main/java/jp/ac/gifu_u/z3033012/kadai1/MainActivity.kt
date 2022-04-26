d1523d90761579ce0af39fe8ec7f6537ed32a263
Keisuke Ishigami <keisuke.130@icloud.com>
2022-04-27 05:50:17 +0900

    dev ver

--- app/src/main/java/jp/ac/gifu_u/z3033012/kadai1/MainActivity.kt
+++ app/src/main/java/jp/ac/gifu_u/z3033012/kadai1/MainActivity.kt
@@ -4,7 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+import java.lang.NumberFormatException
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +24,49 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener {
             Toast.makeText(this, editText.text.toString(), Toast.LENGTH_SHORT).show()
         }
+        val buttonTest = findViewById<Button>(R.id.buttonTest)
+        buttonTest.setOnClickListener {
+            val text = editText.text.toString()
+            getData(text)
+        }
+    }
+
+    private fun getData(text: String) {
+//        lifecycleScope.launch {
+//            val url = "https://api.genderize.io/?name=$text"
+//            val result = backGroundTask(url)
+//            jsonTask(result)
+//        }
+        val name: TextView = findViewById(R.id.name)
+        name.text = text
+    }
+
+    private suspend fun backGroundTask(url: String): String {
+        var result = ""
+        try {
+            val urlObj = URL(url)
+            val br = BufferedReader(InputStreamReader(urlObj.openStream()))
+            result = br.readText()
+        } catch(e: NumberFormatException) {
+            e.printStackTrace()
+        }
+        return result
+    }
+
+    private fun jsonTask(result: String) {
+        val name: TextView = findViewById(R.id.name)
+        val gender: TextView = findViewById(R.id.gender)
+        val probability: TextView = findViewById(R.id.probability)
+        val count: TextView = findViewById(R.id.count)
+        val jsonObj = JSONObject(result)
+        val nameKey = jsonObj.getString("name")
+        val genderKey = jsonObj.getString("gender")
+        val probabilityKey = jsonObj.getString("probability")
+        val countKey = jsonObj.getString("count")
+
+        name.text = nameKey
+        gender.text = genderKey
+        probability.text = probabilityKey
+        count.text = countKey
     }
 }
